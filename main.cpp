80a1fbe18c5125634d431d21584af61ba11471ad
Simon <szymson713@gmail.com>
2023-06-07 08:34:56 +0000

    modified:   main.cpp 	new file:   zad2.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
+
+
 int main() {
-    cout << "Hello World\n";
-    cout << "Input: ";
-    string data;
-    getline(cin, data);
-    cout << "Output: " << data << "\n\n";
+    cout << "Czas: " << jak_szybko(2.5f) << " h\n";
+    cout << "Czas: " << jak_szybko(3.0f) << " h\n";
+    
     return 0;
+}
+
+int jak_szybko(float Czas){
+    float velocity = 1320 / Czas; 
+    
+    return velocity;
 }
